e7004e05905ee51a036db7d559e3a99d5b29951e
Jan <jam@ct.de>
2018-10-19 15:29:23 +0200

    not working first commit

---
+++ getheise.py
@@ -0,0 +1,19 @@
+import feedparser
+d = feedparser.parse('https://www.heise.de/newsticker/heise-atom.xml')
+
+def writeNews(headline, oFile,position):
+    line = position*3 + 9
+    oFile.write("\nOL,"+str(line)+", "+headline[0:40])
+    line = line+1
+    oFile.write("\nOL,"+str(line)+", "+headline[40:80])
+
+template = open("template.txt", "r") 
+header = template.read()
+
+outputfile = open("output.txt", "w")
+
+outputfile.write(header)
+writeNews(d['entries'][0]['title'], outputfile, 0)
+writeNews(d['entries'][1]['title'], outputfile, 1)
+writeNews(d['entries'][2]['title'], outputfile, 2)
+writeNews(d['entries'][3]['title'], outputfile, 3)
